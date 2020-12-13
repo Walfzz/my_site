6dac7610e3207855321cd56be48d209e5c321373
Dayan Lim <walfzzgaming@gmail.com>
2020-12-13 20:36:38 +0800

    etc

--- Website_proj_forced_by_dad/webapp.py
+++ Website_proj_forced_by_dad/webapp.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author ': 'Dayan',
+        'title': 'blog post 1',
+        'content': 'first',
+        'date_posted': '???'
+    },
+    {
+        'author ': 'Dayan2',
+        'title': 'blog post 2',
+        'content': 'second',
+        'date_posted': '???'
+    }
+]
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts = posts)
 
 @app.route('/about')
 def about():
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    with open('file.txt') as f:
+        for line in f:
+            print(line)
+
+main()
